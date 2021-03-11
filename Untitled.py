f8312cd678adf45cba0954ac331f8454f7f51407
AmirKup <73154367+AmirKup@users.noreply.github.com>
2021-03-11 12:50:23 +0600

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+#https://www.hackerrank.com/challenges/arrays-ds/problem
+a = [1,2,3]
+a.reverse()
+a
+
+
+# In[34]:
+
+
+#https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem
+a = [8,20,6,2,19,7,4,15,9,3]
+del a[0]
+del a[-1]
+a
+
+
+# In[38]:
+
+
+a = [3,5,16,12,4,2,5,3,7,3,9,5,5,1,18,3,13]
+for i in reversed(a):
+    print(i)
+
+
+# In[13]:
+
+
+#https://www.hackerrank.com/challenges/tree-preorder-traversal/problem
+a = [     1,
+      
+       2,
+        
+         5,
+        
+       3,    6,
+        
+         4  ]
+a.sort()
+a
+
+
+# In[21]:
+
+
+#https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
+a = [1,2,5,3,6,4]
+a.reverse()
+a
+
+
+# In[22]:
+
+
+#https://www.hackerrank.com/challenges/tree-inorder-traversal/problem
+a = [1,2,
+         5,
+
+       3,    6,
+        
+         4]
+a.sort()
+a
+
+
+# In[ ]:
+
+
+
+
